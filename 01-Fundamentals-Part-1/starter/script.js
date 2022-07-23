4439489c1040c0acbd963ae60b472b154da39687
Ankitanshu <ankitanshu9006@gmail.com>
2022-07-24 00:17:49 +0530

    yy

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,18 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Programmer";
+let mySecondJob = "teacher";
+
+let job1 = "Programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
